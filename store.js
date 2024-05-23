8db6a7b8f198ce72c15ba6cc81098b6368794c30
LinMarc1210 <marc1210899@gmail.com>
2024-05-23 21:37:56 +0800

    新增一些 store 的動畫

--- store.js
+++ store.js
@@ -23,8 +23,55 @@ function blurBg() {
     bg.style.blur = "10px";
 }
 
-function yonexIntro(){
-    var div = document.getElementById("title");
-    console.log(div.style.animation);
-    div.style.animation = "expand 2s linear forwards";
+
+var intro = [
+    $('<table class="title_intro"></table>'),
+    $('<table class="title_intro"></table>'),
+    $('<table class="title_intro"></table>')
+];
+var mainShopItem = [
+    $('<tr><td><img src="src/store/shirt.png">'),
+    $('<tr><td><img src="src/store/shirt.png">'),
+    $('<tr><td><img src="src/store/shirt.png">')
+];
+
+$(intro).each(function(index) {
+    $(this).css({
+        'text-align': 'center',
+        'z-index': '500',
+        'color': 'white',
+        'margin': '0 auto', // 水平置中
+        'align': 'center'
+    });
+    $(this).append(mainShopItem[index]);
+});
+
+function toggleTitle(index) {
+    var checkbox = document.getElementById('toggleCheckbox' + index);
+    var title = document.getElementsByClassName('title')[index - 1];
+    var label = title.querySelector('label div');
+
+    console.log(checkbox.checked)
+    if (checkbox.checked) {
+        title.style.width = '25%';
+        title.style.height = '100%';
+        label.innerHTML = '點我看更多';
+        checkbox.checked = false;
+        $(title).find('.title_intro').remove();
+        $($(title).parent()).find('img').css({'filter': 'blur(0px)', '-webkit-filter:': 'blur(0px)'});
+    } else {
+        title.style.width = '100%';
+        title.style.height = '100%';
+        label.innerHTML = '收起資訊';
+        checkbox.checked = true;
+        if ($(title).find('.title_intro').length === 0){
+            $(title).append(intro[index - 1].clone());  // 確保每次附加的都是新表格
+        }
+        $(title).siblings('img').css({'filter': 'blur(10px)', '-webkit-filter:': 'blur(10px)'});
+    }
 }
+
+
+
+
+
